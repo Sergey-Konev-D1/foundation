a217fec0cadcb50b5875088dcf83b53480986849
Sergey Konev <skon88@mail.ru>
2025-08-12 00:42:52 +0300

    Добавлен код + добавлен новый класс

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("C'mon Rokers!");
-        System.out.println("123");
+        System.out.println("12345");
+    }
+
+    public static void DoIt(){
+        System.out.println("Line");
     }
 }
