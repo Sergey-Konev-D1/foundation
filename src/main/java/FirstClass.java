92dd63fc35eca2b75c6c5cc64ef16b7bd39a5299
Sergey Konev <skon88@mail.ru>
2025-08-13 23:52:53 +0300

    изм логика

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -8,9 +8,6 @@ public class FirstClass {
     }
 
     public static void DoIt(){
-        System.out.println("3");
-        System.out.println("2");
-        System.out.println("1");
-        System.out.println("----");
+        System.out.println("hello UpRokers!");
     }
 }
