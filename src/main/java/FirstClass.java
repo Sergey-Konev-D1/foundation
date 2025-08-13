d9d7e2b81812275291b2bfc7b4677ac9a35dc58d
Sergey Konev <skon88@mail.ru>
2025-08-13 23:10:12 +0300

    Таск 02 + изм код

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,10 +1,13 @@
 public class FirstClass {
     public static void main(String[] args) {
+        System.out.println("!--------!");
         System.out.println("C'mon Rokers!");
         System.out.println("12345");
     }
 
     public static void DoIt(){
-        System.out.println("Line");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
     }
 }
